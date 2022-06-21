ebd035d254cf82c79f10d19db3fe79e32e126ac5
malihitesh2005@gmail.com <malihitesh2005@gmail.com>
2022-06-21 23:50:14 +0530

    updated InterestCalculation_CapitalBuilder SP

--- SQLChanges/InterestCalculation_CapitalBuilder.sql
+++ SQLChanges/InterestCalculation_CapitalBuilder.sql
@@ -1,4 +1,4 @@
-Alter procedure [dbo].[InterestCalculation_CapitalBuilder]
+CREATE procedure [dbo].[InterestCalculation_CapitalBuilder]
 @CustomerProductId uniqueidentifier,
 @Balance decimal(18,4),
 @InterestRate decimal(18,4),
@@ -9,13 +9,29 @@ Alter procedure [dbo].[InterestCalculation_CapitalBuilder]
 As
 Begin
 SET NOCOUNT ON	
-	
-	SELECT @CustomerProductId,@Balance,@InterestRate,@IsFreeze,@CustomerId,@ProductType,@Date
 
-	IF(@IsFreeze = 1)
+	-- it's for debug purpose , can uncomment below statement and test the values
+	--SELECT @CustomerProductId,@Balance,@InterestRate,@IsFreeze,@CustomerId,@ProductType,@Date 
+
+	DECLARE @LastTransactionTime datetime
+	DECLARE @PaymentType int	
+
+	SELECT @LastTransactionTime = max(TransactionTime) from Transactions TR with(nolock)  
+	where TR.CustomerProductId = @CustomerProductId and TR.CustomerId = @CustomerId and TR.[Type] in (1,3,5) 
+	
+
+	Select TOP 1 @PaymentType = PaymentType from CustomerProduct with(nolock) 
+	where CustomerId = @CustomerId and ProductType = @ProductType
+
+	IF ((DATEDIFF(MONTH, @LastTransactionTime, GETDATE())) > 6 and @PaymentType in (2,3)) OR 
+			((DATEDIFF(MONTH, @LastTransactionTime, GETDATE())) > 12 and @PaymentType in (4,5))
 	BEGIN
-		SET @InterestRate = (Select TOP 1 InterestRate from CustomerProduct where CustomerId = @CustomerId and ProductType = @ProductType)
+		SET @InterestRate = 4
 	END
+	ELSE
+		SET @InterestRate = (Select TOP 1 InterestRate from CustomerProduct with(nolock) where CustomerId = @CustomerId and ProductType = @ProductType)
+	END
+	
 	
 	DECLARE @MonthlyTax decimal(18,4)
 	DECLARE @DailyTax decimal(18,4)
@@ -34,7 +50,3 @@ SET NOCOUNT ON
 	END
 
 	SET NOCOUNT OFF 	
-
-End
-
-
